2178515576737e8a850f0a90134a3755926e6e0a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-10-28 15:03:20 +0300

    Move JS from index.html into script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+function getDataAndDrawChart() {
+  const url = "https://raw.githubusercontent.com/python/devguide/main/core-developers/developers.csv";
+  const today = new Date();
+
+  d3.text(url)
+    .then(function(csvText) {
+      // Parse the CSV data into an array of objects
+      let data = d3.csvParseRows(csvText,
+        d3.autoType,
+        function(d) {
+          return d;
+        });
+
+      // Oldest first
+      data.reverse()
+
+      for (let i = 0; i < data.length; i++) {
+        // Remove the last element: "Notes"
+        data[i] = data[i].slice(0, -1);
+        // Set second element to third element
+        data[i][1] = data[i][2].toISOString().slice(0, 10);
+        // If no "Left" date, use today
+        if (data[i][3] === null) {
+          data[i][3] = today;
+        } else {
+          // Append leaving date to second element
+          data[i][1] += " to " + data[i][3].toISOString().slice(0, 10);
+        }
+      }
+      drawChart(data);
+    })
+}
+
 function drawChart(rows) {
   // https://developers.google.com/chart/interactive/docs/gallery/timeline
   let container = document.getElementById("chart_div");
